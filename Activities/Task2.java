488cbd2ddb54a99983a86f8dc0364429ef6d94b3
Carlos Go <69825722+lanlocalarea@users.noreply.github.com>
2025-05-02 07:36:50 +0000

    Task 2 Activity 	new file:   Activities/Task2.java

---
+++ Activities/Task2.java
@@ -0,0 +1,18 @@
+package Activities;
+
+public class Task2 {
+    public static void main(String[] args) {
+        byte a = 0;
+        short b = 3;
+        int c = 1;
+        char d = '1';
+        char e = 'H', 
+             f = 'w', 
+             g = 'd', 
+             h = 'r';
+        boolean i = true;
+        float j = 2.0f;
+
+        System.out.println(e+""+b+""+""+d+""+c+a + " " + f + a + h + c + g + " " + j + " " + i);
+    }
+}
