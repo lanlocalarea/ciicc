dd767a224c718b41ad06372688ba58da43f131dd
Carlos Go <69825722+lanlocalarea@users.noreply.github.com>
2025-05-06 08:04:19 +0000

    String Builder Palindrome Checker 	new file:   ProgrammingChallenge/StringBuilderPalindrome.java

---
+++ ProgrammingChallenge/StringBuilderPalindrome.java
@@ -0,0 +1,22 @@
+package ProgrammingChallenge;
+
+import java.util.Scanner;
+
+public class StringBuilderPalindrome {
+    public static void main(String[] args) {
+        Scanner wordInput = new Scanner(System.in);
+        System.out.println("Palindrome Checker");
+        System.out.print("Enter a word: ");
+        StringBuilder wordStringChecker = new StringBuilder(wordInput.nextLine());
+        String wordHolder = wordStringChecker.toString();
+
+        String result = (wordStringChecker.toString().equals(wordStringChecker.reverse().toString())) ?
+         wordHolder + " is a palindrome" : 
+         wordHolder + " is not a palindrome";
+        wordInput.close();
+        System.out.println(result); 
+
+        
+    }
+    
+}
