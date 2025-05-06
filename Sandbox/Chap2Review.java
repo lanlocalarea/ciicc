82b825913000f6bb5ec4770cdf2859756bd2b59d
Carlos Go <69825722+lanlocalarea@users.noreply.github.com>
2025-05-06 02:06:35 +0000

    Study the flow of the Loop Problem in Long Quiz 	modified:   Sandbox/Chap2Review.java

--- Sandbox/Chap2Review.java
+++ Sandbox/Chap2Review.java
@@ -24,6 +24,13 @@ public class Chap2Review {
             System.out.println();
             }    
         
+            int count = 0;
+            ROW_LOOP: for(int row = 1; row <=3; row++)
+            for(int col = 1; col <=2 ; col++) {
+            if(row * col % 2 == 0) continue ROW_LOOP;
+            count++;
+            }
+            System.out.println(count);
 
     }
 }
