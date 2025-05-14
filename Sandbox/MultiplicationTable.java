f9135fec3077f15b34526d059ab7ca470e8e2900
Carlos Go <69825722+lanlocalarea@users.noreply.github.com>
2025-05-14 06:46:19 +0000

    Recreating Multiplication Table 	new file:   Sandbox/MultiplicationTable.java

---
+++ Sandbox/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package Sandbox;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        
+        for (int a = 1 ; a <= 10; a++) {
+            System.out.print(a);
+
+            for (int b = 2; b <= 10 ; b++){
+                System.out.print("\t" + a*b);
+            }
+            System.out.println("\n");
+        }
+        
+    }
+
+}
