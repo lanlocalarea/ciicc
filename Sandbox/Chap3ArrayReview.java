e7cd5ecacceabe4ad1bcb5e1a50fdd0f617790ef
Carlos Go <69825722+lanlocalarea@users.noreply.github.com>
2025-05-16 18:47:40 +0000

    Testing Codes for Review of Chap 3 and 4.

--- Sandbox/Chap3ArrayReview.java
+++ Sandbox/Chap3ArrayReview.java
@@ -1,4 +1,6 @@
 package Sandbox;
+import static java.util.Arrays.*;
+import static java.util.ArrayList.*;
 
 public class Chap3ArrayReview {
     public static void main(String[] args) {
@@ -6,6 +8,8 @@ public class Chap3ArrayReview {
         Object[] objects = strings;
         String[] againStrings = (String[]) objects;
         System.out.println(againStrings[0]);
+        
+        System.out.println(Chap3and4Review.earthsGravity());
         
 
     }
---
+++ Sandbox/Chap3and4Review.java
@@ -0,0 +1,26 @@
+package Sandbox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chap3and4Review {
+    public static void main(String[] args) {
+
+        //NullPointerException
+        // List heights = new ArrayList<>();
+        // heights.add(null);
+        // Object h = heights.get(0);
+        // System.out.println(h.toString());
+        List numbers = new ArrayList<>();
+        numbers.add(1); numbers.add(2);
+        numbers.remove(1);
+        System.out.println(numbers);
+        System.out.println(earthsGravity());
+
+        
+
+    }
+    public static float earthsGravity(){
+        return 9.8f;
+    }
+}
