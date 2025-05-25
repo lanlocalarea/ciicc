4b45cb2b528b5eda52a3a49adfbf345a3ad963b3
Carlos Go <69825722+lanlocalarea@users.noreply.github.com>
2025-05-25 20:20:55 +0000

    Task 8: Sum of all parameters and cumulative sum of parameters.

---
+++ Activities/Task8.java
@@ -0,0 +1,30 @@
+package Activities;
+
+public class Task8 {
+
+    int SumOfAllParams(int... numbers) {
+       int sumOfParams = 0;
+       int lengthOfParams = numbers.length;
+       int cumulativeSum = 0;
+       
+        for (int number : numbers)
+        sumOfParams += number;
+
+        for (int params = 0; params < lengthOfParams; params++){
+            cumulativeSum = 0;
+            for (int number = numbers[params]; number > 0; number--){
+                cumulativeSum += number;
+            }
+
+            System.out.println("Cumulative sum of " + numbers[params] + " is " + cumulativeSum);
+
+        }
+
+        return sumOfParams;
+
+    }
+    public static void main(String[] args) {
+        Task8 task8 = new Task8();
+        System.out.println("Total of all Parameters: " + task8.SumOfAllParams(4,5,10));
+    }
+}
