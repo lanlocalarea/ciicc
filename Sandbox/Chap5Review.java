0eca559247c38189573e6c38a277136dc235e0df
Carlos Go <69825722+lanlocalarea@users.noreply.github.com>
2025-05-31 15:44:05 +0000

    Files for review

---
+++ Sandbox/Chap5Review.java
@@ -0,0 +1,25 @@
+package Sandbox;
+
+public class Chap5Review extends Bird implements Birdie{
+    public static void main(String[] args) {
+      
+    }
+}
+
+abstract class Bird extends Bird2 {
+
+}
+
+abstract class Bird2  {
+
+}
+
+interface Birdie {
+
+    
+}
+
+interface Birdie2 {
+
+    
+}
---
+++ Sandbox/Chap6Review.java
@@ -0,0 +1,15 @@
+package Sandbox;
+
+import java.io.IOException;
+
+public class Chap6Review {
+    String name;
+    public void nullString(){
+        name.toString();
+    }
+    public static void main(String[] args) {
+        Chap6Review c = new Chap6Review();
+        c.nullString();
+    }
+
+}
